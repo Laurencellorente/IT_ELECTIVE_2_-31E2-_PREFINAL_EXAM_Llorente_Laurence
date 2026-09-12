0b8c35c4a475ede030bc94c4031a049b964ffd4b
Laurencellorente <laurencellorente32@gmail.com>
2026-09-12 17:04:44 +0800

    added Question/Answer model

---
+++ ConsoleApp1/ExamApp/Models/QuestionAnswer.cs
@@ -0,0 +1,10 @@
+﻿namespace IT_ELECTIVE_2_SECTION_PREFINAL_EXAM_Lastname_Firstname.Models
+{
+    public class QuestionAnswer
+    {
+        public int Number { get; set; }
+        public string Question { get; set; } = string.Empty;
+        public string[] Choices { get; set; } = Array.Empty<string>();
+        public string Answer { get; set; } = string.Empty;
+    }
+}
