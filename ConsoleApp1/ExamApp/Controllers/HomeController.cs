b23dabd2c89e28d73c51b1ebd1d6ad0aea432e22
Laurencellorente <laurencellorente32@gmail.com>
2026-09-12 17:12:13 +0800

    q4

--- ConsoleApp1/ExamApp/Controllers/HomeController.cs
+++ ConsoleApp1/ExamApp/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace ExamApp.Controllers
                 new() { Number = 1, Question = "What is the main problem solved by using a database instead of an in-memory collection?", Choices = new[]{"It makes C# code shorter","It prevents the application from restarting","It allows data to persist after the application stops","It removes the need for MVC"}, Answer = "C" },
                 new() { Number = 2, Question = "Which approach is being used when an existing database is used to generate EF Core entity classes?", Choices = new[]{"Code-First","Database-First","Model-First","Controller-First"}, Answer = "B" },
                 new() { Number = 3, Question = "What is the primary purpose of Entity Framework Core?", Choices = new[]{"To create HTML pages automatically","To replace the MVC Controller","To map objects in code to relational database data","To replace the C# compiler"}, Answer = "C" },
+                new() { Number = 4, Question = "Which EF Core component is primarily responsible for communicating with the database?", Choices = new[]{"DbContext","DbSetView","ControllerContext","RazorContext"}, Answer = "A" },
 
 
 
