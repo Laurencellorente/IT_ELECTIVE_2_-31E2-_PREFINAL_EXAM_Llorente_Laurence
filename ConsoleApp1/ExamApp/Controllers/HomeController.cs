20adc3a78a4ebfe792f6e3e5ce7539a2af39415a
Laurencellorente <laurencellorente32@gmail.com>
2026-09-12 17:11:14 +0800

    q2

--- ConsoleApp1/ExamApp/Controllers/HomeController.cs
+++ ConsoleApp1/ExamApp/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace ExamApp.Controllers
                 var answers = new List<QuestionAnswer>
             {
                 new() { Number = 1, Question = "What is the main problem solved by using a database instead of an in-memory collection?", Choices = new[]{"It makes C# code shorter","It prevents the application from restarting","It allows data to persist after the application stops","It removes the need for MVC"}, Answer = "C" },
+                new() { Number = 2, Question = "Which approach is being used when an existing database is used to generate EF Core entity classes?", Choices = new[]{"Code-First","Database-First","Model-First","Controller-First"}, Answer = "B" },
 
 
 
